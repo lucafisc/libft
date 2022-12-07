ffcd1d9e5a4764bcb16c778523e455d09789acec
lucafisc <lucafischer@fastmail.com>
2022-12-07 10:05:14 +0100

    Add ft_lstadd_front

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -2,6 +2,13 @@
 
 void ft_lstadd_front(t_list **lst, t_list *new)
 {
-    
-
+    new->next = *lst;
+    *lst = new;
 }
+
+
+
+    // t_list *pt;
+    // pt = *lst;
+    // new->next = pt;
+    // *lst = new;
