8ad4a43a6ef0f15e0db107e3267e17a400b207d7
lucafisc <lucafischer@fastmail.com>
2022-12-08 16:38:59 +0100

    Fix norm

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,16 +6,16 @@
 /*   By: lde-ross <lde-ross@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 10:27:15 by lde-ross          #+#    #+#             */
-/*   Updated: 2022/12/08 16:33:47 by lde-ross         ###   ########.fr       */
+/*   Updated: 2022/12/08 16:38:32 by lde-ross         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list *new_list;
-	t_list *temp;
+	t_list	*new_list;
+	t_list	*temp;
 
 	new_list = NULL;
 	while (lst)
