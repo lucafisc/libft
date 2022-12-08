983341f0a6ddc6e6997d6bfe743b85f677ddec22
lucafisc <lucafischer@fastmail.com>
2022-12-08 16:37:27 +0100

    All all functions

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -1,19 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lde-ross <lde-ross@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/07 19:51:47 by lde-ross          #+#    #+#             */
-/*   Updated: 2022/12/08 16:36:24 by lde-ross         ###   ########.fr       */
+/*   Created: 2022/12/08 10:27:15 by lde-ross          #+#    #+#             */
+/*   Updated: 2022/12/08 16:33:47 by lde-ross         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstdelone(t_list *lst, void (*del)(void*))
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	del(lst->content);
-	free(lst);
+	t_list *new_list;
+	t_list *temp;
+
+	new_list = NULL;
+	while (lst)
+	{
+		temp = ft_lstnew(f(lst->content));
+		if (!temp)
+		{
+			ft_lstclear(&temp, del);
+			return (0);
+		}
+		ft_lstadd_back(&new_list, temp);
+		lst = lst->next;
+	}
+	return (new_list);
 }
